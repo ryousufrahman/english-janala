c56dc4a0a505f479ffc90fc0caa0a1af08369926
yousufrahman <ryousuf766@gmail.com>
2026-03-03 23:43:07 +0600

    solved conditional rendering

--- scripts/index.js
+++ scripts/index.js
@@ -18,14 +18,29 @@ const loadLessons = ()=>{
      const wordContainer =document.getElementById('word-container')
      wordContainer.innerHTML =''
 
+      if (words.length==0) {
+        
+      wordContainer.innerHTML =`
+          
+       <div class="text-center  col-span-full rounded-xl py-10 space-y-6">
+          <img src="assets/alert-error.png" alt="" class ="mx-auto">
+          
+         <p class="font-bangla text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+         <h2 class="font-bold text-4xl font-bangla">নেক্সট Lesson এ যান</h2>
+    </div>
+      
+      `
+        
+      }
+
      words.forEach(word => {
       const card =document.createElement('div')
       console.log(word);
       card.innerHTML=`
             <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-          <h2 class="font-bold text-2xl">${word.word}</h2>
+          <h2 class="font-bold text-2xl">${word.word ? word.word : ' word not found'}</h2>
         <p class="font-semibold">Meaning /Pronounciation</p>
-         <div class=" font-medium font-bangla"> "${word.meaning} / ${word.pronunciation} "</div>
+         <div class=" font-medium font-bangla"> "${word.meaning ? word.meaning : 'meaning not found'} / ${word.pronunciation ? word.pronunciation :  'pronounciation not found'} "</div>
          <div class="flex justify-between items-center">
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] "><i class="fa-solid fa-circle-info"></i></button>
           <button class="text-black btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
