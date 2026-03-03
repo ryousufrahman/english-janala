18f85298c27f98064184cc2638c9b5f859f7e3f2
yousufrahman <ryousuf766@gmail.com>
2026-03-03 21:01:12 +0600

    created buttons dynamically through api

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+const loadLessons = ()=>{
+    fetch('https://openapi.programming-hero.com/api/levels/all')
+    .then(res => res.json())
+    .then(json=> displayLessons(json.data));
+};
+
+ const loadLevelWord =(id)=>{
+    const url = `
+        https://openapi.programming-hero.com/api/level/${id}
+    `
+    fetch(url)
+    .then(res=> res.json())
+    .then(data=>console.log(data)
+    )
+    
+ }
+
+
+const displayLessons =(lessons)=>{
+  const levelContainer =document.getElementById('level-container')
+  levelContainer.innerHTML ='';
+
+  for(let lesson of lessons){
+    const btnDiv = document.createElement('div');
+    btnDiv.innerHTML=`
+     <button onclick ="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
+    
+    `
+    levelContainer.append(btnDiv)
+  }
+
+}
+loadLessons()
